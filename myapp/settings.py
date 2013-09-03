76c6981a4b187aaa36b199a5802b3e2ca6f44800
Vipin Sharma <vms39@cornell.edu>
2013-09-03 12:04:44 -0400

    trying heroku

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,4 +1,4 @@
-# Django settings for myapp project.
+
 import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -148,3 +148,23 @@ LOGGING = {
         },
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
